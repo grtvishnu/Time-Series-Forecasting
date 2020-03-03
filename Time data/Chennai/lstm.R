824830b1bf453c8dfb09ae3841f0e77f87e8e67b
Vishnu Unnikrishnan <optra7@gmail.com>
2020-03-03 22:36:04 +0530

    new

--- Time data/Chennai/lstm.R
+++ Time data/Chennai/lstm.R
@@ -10,6 +10,8 @@ library(rsample)
 library(yardstick) 
 library(keras)
 library(ggplot2)
+c1 <- read.csv("org_ch.csv")
+c1 <- c1[, c(2,8)]
 View(c1)
 
 p1 <- c1 %>%
@@ -35,21 +37,3 @@ p_title <- ggdraw() +
 
 plot_grid(p_title, p1, p2, ncol = 1, rel_heights = c(0.1, 1, 1))
 
-
-tidy_acf <- function(data, value, lags = 0:20) {
-  
-  value_expr <- enquo(value)
-  
-  acf_values <- data %>%
-    pull(value) %>%
-    acf(lag.max = tail(lags, 1), plot = FALSE) %>%
-    .$acf %>%
-    .[,,1]
-  
-  ret <- tibble(acf = acf_values) %>%
-    rowid_to_column(var = "lag") %>%
-    mutate(lag = lag - 1) %>%
-    filter(lag %in% lags)
-  
-  return(ret)
-}
