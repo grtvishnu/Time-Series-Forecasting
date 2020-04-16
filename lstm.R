dc1fd7b51cc1eac4b959ab7b1977aa875e682d71
Vishnu Unnikrishnan <optra7@gmail.com>
2020-04-17 00:48:38 +0530

    Update lstm.R

--- lstm.R
+++ lstm.R
@@ -2,19 +2,100 @@ library(tidyverse)
 library(keras)
 library(tensorflow)
 library(recommenderlab)
+library(Seurat)
 
 c1 <- read_csv("org_ch.csv")
-c2 <- read_csv("org_dh.csv")
+c2 <- read.csv("org_dh.csv")
 c3 <- read_csv("org_ko.csv")
 c4 <- read_csv("org_hy.csv")
 c5 <- read_csv("org_mu.csv")
 
+c2_op$AQI<- scale(c2_op$AQI, center = T, scale = T)
+
+diffed = diff(c2_op$AQI, differences = 1)
+head(diffed)
 
 
-normalize(x, method = "Z-score")
+lag_transform <- function(x, k= 1){
+  
+  lagged =  c(rep(NA, k), x[1:(length(x)-k)])
+  DF = as.data.frame(cbind(lagged, x))
+  colnames(DF) <- c( paste0('x-', k), 'x')
+  DF[is.na(DF)] <- 0
+  return(DF)
+}
+supervised = lag_transform(diffed, 1)
+head(supervised)
+
+N = nrow(supervised)
+n = round(N *0.7, digits = 0)
+train = supervised[1:n, ]
+test  = supervised[(n+1):N,  ]
 
 
+scale_data = function(train, test, feature_range = c(0, 1)) {
+  x = train
+  fr_min = feature_range[1]
+  fr_max = feature_range[2]
+  std_train = ((x - min(x) ) / (max(x) - min(x)  ))
+  std_test  = ((test - min(x) ) / (max(x) - min(x)  ))
+  
+  scaled_train = std_train *(fr_max -fr_min) + fr_min
+  scaled_test = std_test *(fr_max -fr_min) + fr_min
+  
+  return( list(scaled_train = as.vector(scaled_train), scaled_test = as.vector(scaled_test) ,scaler= c(min =min(x), max = max(x))) )
+  
+}
 
-c2$AQI <- (c2$AQI - mean(c2$AQI)) / sd(c2$AQI)
 
-x <- c2$AQI
+Scaled = scale_data(train, test, c(-1, 1))
+
+y_train = Scaled$scaled_train[, 2]
+x_train = Scaled$scaled_train[, 1]
+
+y_test = Scaled$scaled_test[, 2]
+x_test = Scaled$scaled_test[, 1]
+
+# revet
+invert_scaling = function(scaled, scaler, feature_range = c(0, 1)){
+  min = scaler[1]
+  max = scaler[2]
+  t = length(scaled)
+  mins = feature_range[1]
+  maxs = feature_range[2]
+  inverted_dfs = numeric(t)
+  
+  for( i in 1:t){
+    X = (scaled[i]- mins)/(maxs - mins)
+    rawValues = X *(max - min) + min
+    inverted_dfs[i] <- rawValues
+  }
+  return(inverted_dfs)
+}
+
+
+dim(x_train) <- c(length(x_train), 1, 1)
+
+X_shape2 = dim(x_train)[2]
+X_shape3 = dim(x_train)[3]
+batch_size = 1        
+units = 1 
+
+model <- keras_model_sequential() 
+model%>%
+  layer_lstm(units, batch_input_shape = c(batch_size, X_shape2, X_shape3), stateful= TRUE)%>%
+  layer_dense(units = 1)
+
+model %>% compile(
+  loss = 'mean_squared_error',
+  optimizer = optimizer_adam( lr= 0.02, decay = 1e-6 ),  
+  metrics = c('accuracy')
+)
+
+summary(model)
+
+Epochs = 50   
+for(i in 1:Epochs ){
+  model %>% fit(x_train, y_train, epochs=1, batch_size=batch_size, verbose=1, shuffle=FALSE)
+  model %>% reset_states()
+}
